491adaafe47c0acb3bbb633bada00ad1324bf2ee
Min <minkim4568>
2016-10-28 17:39:38 -0400

    comments
    comments

--- app/services/category.js
+++ app/services/category.js
@@ -1,30 +1,41 @@
 angular.module('productApp')
 // CategoryService returning list of categories and category lookup by id
-    .service('categoryService', function($http, $q, $window) {
+    .service('categoryService', function($http, $q) {
+
+        // service initialization
         var self = this;
+
+        // service method 1: get all categories
         self.getAllCategories = function () {
             var deferred = $q.defer();
             setTimeout(function() {
                 $http.get('../app/res/data.json')
                     .then(function (result) {
+                        // categories found
                         deferred.resolve(result.data.categories);
                     }, function (reason) {
+                        // not found
                         deferred.reject(reason);
                     });
             }, 100);
             return deferred.promise;
         };
+
+        // service method 2: get a category by id
         self.getCategoryById = function(categoryId) {
             var deferred = $q.defer();
             setTimeout(function() {
                 $http.get('../app/res/data.json')
                     .then(function (result) {
+                        // category search loop
                         for(var i=0; i<result.data.categories.length; i++) {
                             if(categoryId == result.data.categories[i]._id ) {
+                                // category found
                                 deferred.resolve(result.data.categories[i]);
                             }
                         }
                     }, function (reason) {
+                        // not found
                         deferred.reject(reason);
                     });
             }, 100);
