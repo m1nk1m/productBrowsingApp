8efb0c649499989b1a7fb289b51e1355c5c191c2
Min <minkim4568>
2016-10-28 02:20:45 -0400

    d
    dd

--- app/app.js
+++ app/app.js
@@ -3,23 +3,3 @@
  */
 
 angular.module('productApp', ['ui.router']);
-    // .config(function ($stateProvider, $urlRouterProvider, categoryService) {
-    //     var categoryPage = {
-    //         name: 'category',
-    //         url: '/category/{categoryId}',
-    //         component: 'category',
-    //         resolve: {
-    //             category: function(categoryService) {
-    //                 return categoryService.get();
-    //             }
-    //         },
-    //         template: '../view/templates/category.html'
-    //
-    //     };
-    //
-    //     var productPage = {
-    //
-    //     };
-    //
-    //
-    // });
